1b1faa762a55f6f81e71cb648093b398d9f3c296
DESKTOP-1JNEHFN\Crummorn <igorcaze@gmail.com>
2019-04-29 11:52:58 -0300

    Reformulado view de formularios.

--- view/admin/fornecedor/fragments/form-base.php
+++ view/admin/fornecedor/fragments/form-base.php
@@ -1,25 +1,27 @@
-<div class="form-group">
-    <label class="control-label" for="cnpj">CNPJ *</label>
-    <input type="text" class="form-control" name="cnpj" autofocus="autofocus" 
+<div class="row">
+    <div class="form-group col-md-4">
+        <label class="control-label" for="cnpj">CNPJ *</label>
+        <input type="text" class="form-control" name="cnpj" autofocus="autofocus" 
         placeholder="Informe o CNPJ" value="<?= $fornecedor['cnpj'] ?>">
-</div>
-
-<div class="form-group">
-    <label class="control-label" for="nome">Nome *</label>
-    <input type="text" class="form-control" name="nome"
+    </div>
+    
+    <div class="form-group col-md-4">
+        <label class="control-label" for="nome">Nome *</label>
+        <input type="text" class="form-control" name="nome"
         placeholder="Informe o Nome" value="<?= $fornecedor['nome'] ?>">
+    </div>
+    
+    <div class="form-group col-md-4">
+        <label class="control-label" for="contato">Contato</label>
+        <input type="text" class="form-control" name="contato" 
+            placeholder="Informe o Nome" value="<?= $fornecedor['contato'] ?>">
+    </div>
 </div>
 
-<div class="form-group">
-    <label class="control-label" for="endereco">Endereco *</label>
+<div class="form-group ">
+    <label class="control-label" for="endereco">Endereco</label>
     <input type="text" class="form-control" name="endereco" 
-        placeholder="Informe o Endereco" value="<?= $fornecedor['endereco'] ?>">
-</div>
-
-<div class="form-group">
-    <label class="control-label" for="contato">Contato *</label>
-    <input type="text" class="form-control" name="contato" 
-        placeholder="Informe o Nome" value="<?= $fornecedor['contato'] ?>">
+    placeholder="Informe o Endereco" value="<?= $fornecedor['endereco'] ?>">
 </div>
 
 <div class="form-group row">
--- view/admin/produto/fragments/form-base.php
+++ view/admin/produto/fragments/form-base.php
@@ -1,44 +1,42 @@
-<div class="form-group">
-    <label class="control-label" for="nome">Nome *</label>
-    <input type="text" class="form-control" name="nome"
-        placeholder="Informe o Nome" value="<?= $produto['nome'] ?>">
+<div class="form-row">
+    <div class="form-group col-md-8">
+        <label class="control-label" for="nome">Nome *</label>
+        <input type="text" class="form-control" name="nome" autofocus="autofocus" 
+            placeholder="Informe o Nome" value="<?= $produto['nome'] ?>">
+    </div>
+    
+    <div class="form-group col-md-4">
+        <label class="control-label" for="peso">Peso</label>
+        <input type="text" class="form-control" name="peso" 
+            placeholder="Informe o Peso" value="<?= $produto['peso'] ?>">
+    </div>
 </div>
 
-<div class="form-group">
-    <label class="control-label" for="peso">Peso *</label>
-    <input type="text" class="form-control" name="peso" 
-        placeholder="Informe o Peso" value="<?= $produto['peso'] ?>">
+<div class="form-row">
+    <div class="form-group col-md-4">
+        <label class="control-label" for="estoque">Estoque</label>
+        <input type="text" class="form-control" name="estoque" 
+            placeholder="Informe o Estoque" value="<?= $produto['estoque'] ?>" <?= campoVazio($produto['estoque'])?>>
+    </div>
+    
+    <div class="form-group col-md-4">
+        <label class="control-label" for="valor">Valor *</label>
+        <input type="text" class="form-control" name="valor" 
+            placeholder="Informe o Valor" value="<?= $produto['valor'] ?>">
+    </div>
+    
+    <div class="form-group col-md-4">
+        <label class="control-label" for="custo">Custo</label>
+        <input type="text" class="form-control" name="custo" 
+            placeholder="Informe o Custo" value="<?= $produto['custo'] ?>">
+    </div>
 </div>
-
-<div class="form-group">
-    <label class="control-label" for="descricao">Descricao *</label>
-    <textarea class="form-control" name="descricao" 
-        placeholder="Informe a Descrição"><?= $produto['descricao'] ?></textarea>
-</div>
-
-<div class="form-group">
-    <label class="control-label" for="estoque">Estoque *</label>
-    <input type="text" class="form-control" name="estoque" 
-        placeholder="Informe o Estoque" value="<?= $produto['estoque'] ?>" <?= campoVazio($produto['estoque'])?>>
-</div>
-
-<div class="form-group">
-    <label class="control-label" for="valor">Valor *</label>
-    <input type="text" class="form-control" name="valor" 
-        placeholder="Informe o Valor" value="<?= $produto['valor'] ?>">
-</div>
-
-<div class="form-group">
-    <label class="control-label" for="custo">Custo *</label>
-    <input type="text" class="form-control" name="custo" 
-        placeholder="Informe o Custo" value="<?= $produto['custo'] ?>">
-</div>
-
-<tr>
-    <td>Categoria</td>
-    <td>
+    
+<div class="form-row">
+    <div class="form-group col-md-6">
+        <label class="control-label" for="categoria">Categoria *</label>
         <select class="form-control" name="categoria_id">
-            <?php foreach($categorias as $categoria) :
+            <?php foreach($categorias as $categoria) : 
                 $essaEhACategoria = $produto['categoria_id'] == $categoria['id'];
                 $selecao = $essaEhACategoria ? "selected='selected'" : "";
             ?>
@@ -47,12 +45,10 @@
                 </option>
             <?php endforeach ?>
         </select>
-    </td>
-</tr>
+    </div>
 
-<tr>
-    <td>Fornecedor</td>
-    <td>
+    <div class="form-group col-md-6">
+        <label class="control-label" for="fornecedor">fornecedor *</label>
         <select class="form-control" name="fornecedor_id">
             <?php foreach($fornecedores as $fornecedor) :
                 $essaEhOFornecedor = $produto['fornecedor_id'] == $fornecedor['id'];
@@ -63,8 +59,14 @@
                 </option>
             <?php endforeach ?>
         </select>
-    </td>
-</tr>
+    </div>
+</div>
+
+<div class="form-group">
+    <label class="control-label" for="descricao">Descricao</label>
+    <textarea class="form-control" name="descricao" 
+        placeholder="Informe a Descrição"><?= $produto['descricao'] ?></textarea>
+</div>
 
 <div class="form-group row">
     <div class="col-md-12 ">
