2545696abf8572933d0e9ba66629080d264a8f26
DESKTOP-1JNEHFN\Crummorn <igorcaze@gmail.com>
2019-05-02 01:32:48 -0300

    Refatoração de codigo (Produto)
    Removido todo SQL do Controller e alocado no Service.

--- view/admin/database/produto-controller.php
+++ view/admin/database/produto-controller.php
@@ -2,6 +2,8 @@
 require_once ("conecta.php");
 require_once ("produto-service.php");
 
+$produtoService = new ProdutoService();
+
 function insereProduto($conexao, $nome, $valor, $descricao, $peso, $estoque, $custo, $categoria_id, $fornecedor_id) {
     $nome = mysqli_real_escape_string($conexao, $nome);
     $descricao = mysqli_real_escape_string($conexao, $descricao);
@@ -14,8 +16,7 @@ function insereProduto($conexao, $nome, $valor, $descricao, $peso, $estoque, $cu
         die(); 
     }
 
-    $service = new ProdutoService();
-    return $service->adicionar($conexao, $nome, $valor, $descricao, $peso, $estoque, $custo, $categoria_id, $fornecedor_id);
+    return $GLOBALS['produtoService']->adicionar($conexao, $nome, $valor, $descricao, $peso, $estoque, $custo, $categoria_id, $fornecedor_id);
 }
 
 function alteraProduto($conexao, $id, $nome, $valor, $descricao, $peso, $estoque, $custo, $categoria_id, $fornecedor_id) {
@@ -30,10 +31,58 @@ function alteraProduto($conexao, $id, $nome, $valor, $descricao, $peso, $estoque
         die(); 
     }
 
-    $service = new ProdutoService();
-    return $service->alterar($conexao, $id, $nome, $valor, $descricao, $peso, $estoque, $custo, $categoria_id, $fornecedor_id);
+    return $GLOBALS['produtoService']->alterar($conexao, $id, $nome, $valor, $descricao, $peso, $estoque, $custo, $categoria_id, $fornecedor_id);
 }
 
+function removeProduto($conexao, $id) {
+    return $GLOBALS['produtoService']->remover($conexao, $id);
+}
+
+function listaProdutos($conexao) {
+    return $GLOBALS['produtoService']->listaProdutos($conexao);
+}
+
+function buscaProduto($conexao, $id) {
+    return $GLOBALS['produtoService']->buscaProduto($conexao, $id);
+}
+
+function totalProdutos($conexao) {
+    return $GLOBALS['produtoService']->totalProdutos($conexao);
+}
+
+function totalEstoqueProdutos($conexao) {
+    return $GLOBALS['produtoService']->totalEstoqueProdutos($conexao);
+}
+
+function possivelLucroDeUmProdutoEspecifico($conexao, $id) {
+    return $GLOBALS['produtoService']->possivelLucroDeUmProdutoEspecifico($conexao, $id);
+}
+
+function totalEstoqueProdutosFornecedorEspecifico($conexao, $fornecedor_id) {
+    return $GLOBALS['produtoService']->totalEstoqueProdutosFornecedorEspecifico($conexao, $fornecedor_id);
+}
+ 
+function adicionarEstoque($conexao, $id, $quantidade) {
+    $produto = buscaProduto($conexao, $id);
+    return alteraProduto($conexao, $id, $produto['nome'], $produto['valor'], $produto['descricao'], $produto['peso'], ($produto['estoque'] + $quantidade), $produto['custo'], $produto['categoria_id'], $produto['fornecedor_id']);
+}
+
+function darBaixaNoEstoque($conexao, $id, $quantidade) {
+    $produto = buscaProduto($conexao, $id);
+    if ($produto['estoque'] > $quantidade) {
+        return alteraProduto($conexao, $id, $produto['nome'], $produto['valor'], $produto['descricao'], $produto['peso'], ($produto['estoque'] - $quantidade), $produto['custo'], $produto['categoria_id'], $produto['fornecedor_id']);
+    } else {
+        $produto = buscaProduto($conexao, $id);     
+        $_SESSION['alertType'] = 'danger';
+        $_SESSION['alertMsg'] = 'Produto ' . $produto['nome'] . ' não tem estoque suficiente!';
+        header("Location: ../listagem.php");    
+        die();
+    }
+}
+
+/*
+ * Funções de Utilidade
+ */
 function validaProduto($nome, $descricao, $valor) {
     $listaErros = array();
 
@@ -63,78 +112,4 @@ function adicionaValoresProdutoSession($nome, $valor, $descricao, $peso, $estoqu
     $_SESSION['fornecedor_id'] = $fornecedor_id;
     $_SESSION['listaErros'] = $listaErros;
     $_SESSION['alertType'] = 'danger';
-}
-
-function removeProduto($conexao, $id) {
-    $serviceProduto = new ProdutoService();
-    return $serviceProduto->remover($conexao, $id);
-}
-
-function listaProdutos($conexao) {
-    $produtos = array();
-    $query = "SELECT produto.*, c.nome AS categoria_nome, f.nome AS fornecedor_nome   
-                FROM produto 
-                LEFT JOIN categoria AS c ON c.id=produto.categoria_id
-                LEFT JOIN fornecedor AS f ON f.id=produto.fornecedor_id";
-    $resultado = mysqli_query($conexao, $query);
-    while($produto = mysqli_fetch_assoc($resultado)) {
-        array_push($produtos, $produto);
-    }
-    return $produtos;
-}
-
-function buscaProduto($conexao, $id) {
-    $query = "SELECT p.*   
-                FROM produto AS p
-                WHERE id = {$id}";
-    $resultado = mysqli_query($conexao, $query);
-    return mysqli_fetch_assoc($resultado);
-}
-
-function totalProdutos($conexao) {
-    $query = "SELECT * FROM produto";
-    $resultado = mysqli_query($conexao, $query);
-    return $resultado->num_rows;
-}
-
-function totalEstoqueProdutos($conexao) {
-    $query = "SELECT SUM(estoque) AS total_estoque FROM produto";
-    $resultado = mysqli_query($conexao, $query);
-    $row = mysqli_fetch_object($resultado);
-    return $row->total_estoque;
-}
-
-function possivelLucroDeUmProdutoEspecifico($conexao, $id) {
-    $query = "SELECT * FROM produto WHERE id = {$id}";
-    $resultado = mysqli_query($conexao, $query);
-    $row = mysqli_fetch_object($resultado);
-    return ($row->valor - $row->custo) * $row->estoque;
-}
-
-function totalEstoqueProdutosFornecedorEspecifico($conexao, $fornecedor_id) {
-    $query = "SELECT SUM(estoque) AS total_estoque 
-                FROM produto AS p
-                LEFT JOIN fornecedor AS f ON f.id = p.fornecedor_id 
-                WHERE p.fornecedor_id = $fornecedor_id";
-    $resultado = mysqli_query($conexao, $query);
-    $row = mysqli_fetch_object($resultado);
-    return $row->total_estoque;
-}
-
-function adicionarEstoque($conexao, $id, $quantidade) {
-    $produto = buscaProduto($conexao, $id);
-    return alteraProduto($conexao, $id, $produto['nome'], $produto['valor'], $produto['descricao'], $produto['peso'], ($produto['estoque'] + $quantidade), $produto['custo'], $produto['categoria_id'], $produto['fornecedor_id']);
-}
-
-function darBaixaNoEstoque($conexao, $id, $quantidade) {
-    $produto = buscaProduto($conexao, $id);
-    if ($produto['estoque'] > $quantidade) {
-        return alteraProduto($conexao, $id, $produto['nome'], $produto['valor'], $produto['descricao'], $produto['peso'], ($produto['estoque'] - $quantidade), $produto['custo'], $produto['categoria_id'], $produto['fornecedor_id']);
-    } else {
-        $produto = buscaProduto($conexao, $id);     
-        $_SESSION['alertType'] = 'danger';
-        $_SESSION['alertMsg'] = 'Produto ' . $produto['nome'] . ' não tem estoque suficiente!';
-        header("Location: ../listagem.php");    
-        die();
-    }
 }
--- view/admin/database/produto-service.php
+++ view/admin/database/produto-service.php
@@ -20,6 +20,55 @@ class ProdutoService {
         $query = "DELETE FROM produto WHERE id = {$id}";
         return mysqli_query($conexao, $query);
     }
+
+    function listaProdutos($conexao) {
+        $produtos = array();
+        $query = "SELECT produto.*, c.nome AS categoria_nome, f.nome AS fornecedor_nome   
+                    FROM produto 
+                    LEFT JOIN categoria AS c ON c.id=produto.categoria_id
+                    LEFT JOIN fornecedor AS f ON f.id=produto.fornecedor_id";
+        $resultado = mysqli_query($conexao, $query);
+        while($produto = mysqli_fetch_assoc($resultado)) {
+            array_push($produtos, $produto);
+        }
+        return $produtos;
+    }
+
+    function buscaProduto($conexao, $id) {
+        $query = "SELECT p.* FROM produto AS p WHERE id = {$id}";
+        $resultado = mysqli_query($conexao, $query);
+        return mysqli_fetch_assoc($resultado);
+    }
+    
+    function totalProdutos($conexao) {
+        $query = "SELECT * FROM produto";
+        $resultado = mysqli_query($conexao, $query);
+        return $resultado->num_rows;
+    }
+    
+    function totalEstoqueProdutos($conexao) {
+        $query = "SELECT SUM(estoque) AS total_estoque FROM produto";
+        $resultado = mysqli_query($conexao, $query);
+        $row = mysqli_fetch_object($resultado);
+        return $row->total_estoque;
+    }
+
+    function possivelLucroDeUmProdutoEspecifico($conexao, $id) {
+        $query = "SELECT * FROM produto WHERE id = {$id}";
+        $resultado = mysqli_query($conexao, $query);
+        $row = mysqli_fetch_object($resultado);
+        return ($row->valor - $row->custo) * $row->estoque;
+    }
+
+    function totalEstoqueProdutosFornecedorEspecifico($conexao, $fornecedor_id) {
+        $query = "SELECT SUM(estoque) AS total_estoque 
+                    FROM produto AS p
+                    LEFT JOIN fornecedor AS f ON f.id = p.fornecedor_id 
+                    WHERE p.fornecedor_id = $fornecedor_id";
+        $resultado = mysqli_query($conexao, $query);
+        $row = mysqli_fetch_object($resultado);
+        return $row->total_estoque;
+    }
 }
 
 ?>
